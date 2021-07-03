6c97e69debe0f73132c37f5002047938ea47c8b1
karolis-mrc <karolismarock@gmail.com>
2021-07-03 16:35:07 +0300

    proxy test

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-const proxy = require("http-proxy-middleware");
-
-module.exports = function(app) {
-  app.use(
-    app.use(proxy('/keywords', { target: 'http://localhost:5000' })),
-    app.use(proxy('/articles', { target: 'http://localhost:5000' }))
-  );
-};
