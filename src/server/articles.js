12dcc21fa0828b17392dd7210f8161e5694f782b
karolis-mrc <karolismarock@gmail.com>
2021-07-04 10:48:32 +0300

    database connected to store keywords and clicked article details

--- src/server/articles.js
+++ src/server/articles.js
@@ -1,6 +1,9 @@
+const {storeArticle} = require('../services/database');
+
 const log = (item) => {
   const { title, url } = item;
   console.log(`User clicked on this article: ${title}. Article url is: ${url}`);
+  storeArticle(item);
 };
 
 module.exports = { log };
--- src/server/keywords.js
+++ src/server/keywords.js
@@ -1,5 +1,8 @@
+const {storeKeywords} = require('../services/database');
+
 const log = (item) => {
   console.log(`User's search keyword: ${item}`);
+  storeKeywords(item);
 };
 
 module.exports = { log };
